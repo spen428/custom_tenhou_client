bb1722ce2d8513b4281170c7f4a94c3cc6e86971
spen428 <spen428@users.noreply.github.com>
2017-02-11 04:08:52 +0000

    init

---
+++ tenhou/main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import logging
+import socket
+
+from tenhou.client import TenhouClient
+from utils.settings_handler import settings
+from tenhou.gui.main import Gui
+
+
+logger = logging.getLogger('tenhou')
+
+
+def connect_and_play():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((settings.TENHOU_HOST, settings.TENHOU_PORT))
+
+    logger.info('Bot AI enabled: {}'.format(settings.ENABLE_AI))
+
+    client = TenhouClient(s)
+
+    try:
+        was_auth = client.authenticate()
+
+        if was_auth:
+            client.start_game()
+        else:
+            client.end_game()
+    except KeyboardInterrupt:
+        logger.info('Ending the game... can take 15 seconds')
+        client.end_game()
+
+
+def start_client():
+    gui = Gui()
+    gui.run()
+
